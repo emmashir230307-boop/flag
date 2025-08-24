356047d5a0b2965db599967f8e7d41e4d28ddd9d
jbt <danielle.mofkadi@gmail.com>
2025-08-24 14:38:18 +0300

    commit

--- game_field.py
+++ game_field.py
@@ -1,12 +1,33 @@
 import consts
+import random
 
 board=[]
 
 def create_board(board):
-    for i in range(consts.SCREEN_SIZE[0]):
+    for i in range(consts.SCREEN_SIZE[1]):
         temp=[]
-        for j in range(consts.SCREEN_SIZE[1]):
+        for j in range(consts.SCREEN_SIZE[0]):
             temp.append(consts.EMPTY)
         board.append(temp)
+    for i in range(4):
+        for j in range(2):
+            board[i][j]=consts.SOLDIER
+    for i in range(consts.SCREEN_SIZE[1]-3,consts.SCREEN_SIZE[1]):
+        for j in range(consts.SCREEN_SIZE[0]-4,consts.SCREEN_SIZE[0]):
+            board[i][j]=consts.FLAG
+
+def insert_mines(board):
+    for i in range(consts.MINE_NUM):
+        row=random.randint(0,consts.SCREEN_SIZE[1]-1)
+        col=random.randint(0,consts.SCREEN_SIZE[0]-3)
+        while consts.MINE in board[row][col:col+3] or consts.SOLDIER in board[row][col:col+3] or consts.FLAG in board[row][col:col+3]:
+            row = random.randint(0, consts.SCREEN_SIZE[1] - 1)
+            col = random.randint(0, consts.SCREEN_SIZE[0] - 3)
+        print(row,col)
+        board[row][col:col + 3] = [consts.MINE,consts.MINE,consts.MINE]
+
+
 create_board(board)
-print(board)
+insert_mines(board)
+for i in board:
+    print(i)
