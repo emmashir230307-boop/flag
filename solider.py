b8666942c8a0aa5012528c582ecf8111087e82e5
jbt <michalrosh7@gmail.com>
2025-08-24 15:51:24 +0300

    screen

--- solider.py
+++ solider.py
@@ -9,5 +9,13 @@ def legs_location():
     pass
 
 
-def body_location():
+def body_location(location):
+    soldier_location=make_soldier(location)['Location']
+    row=int(soldier_location[0])
+    col=int(soldier_location[1])
+    init_tpl=(row,col)
     body_loc_lst=[]
+    for i in range(consts.SOLDIER_SIZE[1]):
+        for k in range(consts.SOLDIER_SIZE[0]):
+            loc_tpl=(init_tpl[0]+k,init_tpl[1]+i)
+            body_loc_lst.append(loc_tpl)
