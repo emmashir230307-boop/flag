9969f40914fdcb9717ee0c0219162689ec31f7dd
jbt <michalrosh7@gmail.com>
2025-08-24 15:36:55 +0300

    screen

---
+++ solider.py
@@ -0,0 +1,13 @@
+import consts
+
+def make_soldier(soldier_location):
+    soldier={'Image':consts.SOLDIER_IMG,
+             'Location': soldier_location}
+    return soldier
+
+def legs_location():
+    pass
+
+
+def body_location():
+    body_loc_lst=[]
